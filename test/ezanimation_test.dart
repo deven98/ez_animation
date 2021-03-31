666bf1dd9f954766868c8d6b86698435b5fc2fce
Deven Joshi <deven9852@gmail.com>
2021-03-31 12:16:37 +0530

    added tests

--- test/ezanimation_test.dart
+++ test/ezanimation_test.dart
@@ -1,5 +1,47 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ezanimation/ezanimation.dart';
+import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  testWidgets('tests start of animation', (tester) async {
+    EzAnimation ezAnimation = EzAnimation(0.0, 100.0, Duration(seconds: 1));
+
+    await tester.pumpWidget(Container());
+
+    expect(ezAnimation.value, 0.0);
+  });
+
+  testWidgets('tests middle of animation', (tester) async {
+    EzAnimation ezAnimation = EzAnimation(0.0, 100.0, Duration(seconds: 1));
+    ezAnimation.start();
+
+    await tester.pumpWidget(Container());
+    await tester.pump(Duration(milliseconds: 500));
+    ezAnimation.stop();
+
+    expect(ezAnimation.value, 50.0);
+  });
+
+  testWidgets('tests completion of animation', (tester) async {
+    EzAnimation ezAnimation = EzAnimation(0.0, 100.0, Duration(seconds: 1));
+    ezAnimation.start();
+
+    await tester.pumpWidget(Container());
+    await tester.pump(Duration(seconds: 2));
+
+    expect(ezAnimation.value, 100.0);
+  });
+
+  testWidgets('tests middle of curved animation', (tester) async {
+    EzAnimation ezAnimation =
+        EzAnimation(0.0, 100.0, Duration(seconds: 1), curve: Curves.bounceIn);
+    ezAnimation.start();
+
+    await tester.pumpWidget(Container());
+    await tester.pump(Duration(milliseconds: 500));
+    ezAnimation.stop();
+
+    expect(ezAnimation.value, isNot(equals(50.0)));
+  });
+}
